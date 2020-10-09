fc8c238f0e200c411091e4d7f263224e56ebceab
castelll0009 <45212708+castelll0009@users.noreply.github.com>
2020-10-08 22:22:24 -0500

    Initial commit

---
+++ src/main/java/control/Road_To_Sun.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoClientURI;
+import com.mongodb.MongoCredential;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.MongoClient;
+import com.mongodb.ServerAddress;
+//import com.mongodb.client.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+import com.mongodb.client.MongoDatabase;
+import java.util.Arrays;
+
+/**
+ *
+ * @author equipo
+ */
+
+
+public class Road_To_Sun {
+      //com.mongodb.client.MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
+ MongoClient mongoClient;
+    MongoClientURI uri = new
+    MongoClientURI("mongodb://userLab3:passworduserLab3@93.188.167.110:27017/?authSource=lab3");
+    MongoDatabase db;
+   
+
+     public String consultarCientes(){
+   //  return "{\"clave\":\"Hola mundo\"}";  //construccion Json
+    mongoClient = new MongoClient(uri);
+    db = mongoClient.getDatabase("lab3");
+   MongoCollection<Document> collection = db.getCollection("usuarios");
+
+   return  collection.find().first().toJson();
+ 
+    }
+     
+     
+  
+    
+}
